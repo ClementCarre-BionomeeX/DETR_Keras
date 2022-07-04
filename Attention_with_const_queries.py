0599c87be013461c81f60afd43ae1a471aebfe4b
Clément Carré <clement.carre@bionomeex.com>
2022-07-04 16:27:25 +0200

    Black file

--- Attention_with_const_queries.py
+++ Attention_with_const_queries.py
@@ -1,6 +1,7 @@
 # %%
 import tensorflow as tf
 
+
 # %%
 class Attention_with_const_queries(tf.keras.layers.Layer):
     def __init__(self, nqueries, nheads, *args, **kwargs):
@@ -14,32 +15,36 @@ class Attention_with_const_queries(tf.keras.layers.Layer):
             name="queries",
             shape=(self.nqueries, self.nqueries),
             initializer="orthogonal",
-            trainable=True
+            trainable=True,
         )
         self.proj = tf.keras.layers.Dense(input_shape[-1])
         self.proj.build((self.nqueries, self.nqueries))
         self.attention = tf.keras.layers.MultiHeadAttention(
-            num_heads=self.nheads,
-            key_dim=input_shape[-1] // self.nheads
+            num_heads=self.nheads, key_dim=input_shape[-1] // self.nheads
         )
         self.attention._build_from_signature(
             query=(None, self.nqueries, input_shape[-1]),
             key=input_shape,
-            value=input_shape
+            value=input_shape,
         )
 
     def get_config(self):
         config = super().get_config()
-        config.update({
-            "nqueries" : self.nqueries,
-            "nheads" : self.nheads,
-        })
+        config.update(
+            {
+                "nqueries": self.nqueries,
+                "nheads": self.nheads,
+            }
+        )
         return config
 
     def call(self, inputs):
         return self.attention(
-            query=tf.repeat([self.proj(self.w)], [tf.shape(inputs)[0]], axis=0), key=inputs, value=inputs
+            query=tf.repeat([self.proj(self.w)], [tf.shape(inputs)[0]], axis=0),
+            key=inputs,
+            value=inputs,
         )
+
 
 # %%
 if __name__ == "__main__":
@@ -63,9 +68,10 @@ if __name__ == "__main__":
     model.save("test.hdf5")
 
     # %%
-    model2 = tf.keras.models.load_model("test.hdf5", custom_objects={
-        "Attention_with_const_queries": Attention_with_const_queries
-    })
+    model2 = tf.keras.models.load_model(
+        "test.hdf5",
+        custom_objects={"Attention_with_const_queries": Attention_with_const_queries},
+    )
 
     # %%
     pred2 = model2(data)
@@ -85,11 +91,8 @@ if __name__ == "__main__":
     data_X = np.random.uniform(0, 1, (200, 11, 8))
     data_Y = np.random.uniform(0, 1, (200, 17, 1))
 
-
     # %%
-    model.fit(
-        data_X, data_Y, epochs=200, batch_size=20
-    )
+    model.fit(data_X, data_Y, epochs=200, batch_size=20)
     # %%
 
     pred3 = model(data)
@@ -101,9 +104,10 @@ if __name__ == "__main__":
     model.save("test2.hdf5")
     # %%
 
-    model2 = tf.keras.models.load_model("test2.hdf5", custom_objects={
-        "Attention_with_const_queries": Attention_with_const_queries
-    })
+    model2 = tf.keras.models.load_model(
+        "test2.hdf5",
+        custom_objects={"Attention_with_const_queries": Attention_with_const_queries},
+    )
     # %%
 
     pred4 = model2(data)
